fee31112ade5a634dc4cf3dc390ab7cf0db3b74e
JayaneEllen <jayannedias79@gmail.com>
2025-12-02 23:34:43 -0300

    Add files via upload

---
+++ painel.php
@@ -0,0 +1,9 @@
+<script rel="stylesheet" href="style.css"></script>
+<script rel="stylesheet" href="styleconteudo.css"></script>
+<script rel="stylesheet" href="stylerodape.css"></script>
+
+<?php
+
+include_once "topo.php";
+include_once "menu.php";
+include_once "rodape.php";
